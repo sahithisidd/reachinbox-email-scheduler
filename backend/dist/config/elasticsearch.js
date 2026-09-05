ad664365668069cbcd6f693f2016a10bb8c3e048
Sahithisidd <sahithisidd@gmail.com>
2026-09-05 15:47:07 +0530

    Fix deployment files

--- backend/dist/config/elasticsearch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMAIL_INDEX = exports.elasticsearch = void 0;
-const elasticsearch_1 = require("@elastic/elasticsearch");
-exports.elasticsearch = new elasticsearch_1.Client({
-    node: process.env.ELASTICSEARCH_URL || "http://localhost:19200",
-});
-exports.EMAIL_INDEX = "emails";
